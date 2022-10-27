cab17baacc8f2f803dc6061c169e6a356071532b
Patricia <fdspaty@hotmail.com>
2022-10-27 20:28:32 -0300

    alteracao

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "ola mundo"
+    return "Deu certo"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
+.
